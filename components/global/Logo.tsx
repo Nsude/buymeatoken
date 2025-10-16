189457a8194bd4b26e3ceec778e7f2af03c824db
Meshach Nsude <nsudeme777@gmail.com>
2025-10-16 01:29:56 +0100

    update favicon

--- components/global/Logo.tsx
+++ components/global/Logo.tsx
@@ -1,10 +1,7 @@
-import LogoTokenIcon from "../../public/icons/LogoTokenIcon";
-
 
 export default function Logo() {
   return (
     <div className=" flex items-center gap-x-2">
-      <LogoTokenIcon size={22} color="" />
       <h3 className="font-medium text-[1rem]">buymeatoken</h3> 
     </div>
   )
