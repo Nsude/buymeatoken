8e05e98e7162bd207da549e22a37542dbd0541a8
Meshach Nsude <nsudeme777@gmail.com>
2025-10-24 14:15:32 +0100

    create user and sign message implemented

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -5,20 +5,55 @@ import Logo from "../../components/global/Logo";
 import { useEffect } from "react";
 import { useWallet } from "@solana/wallet-adapter-react";
 import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+import bs58 from "bs58";
 
 export default function SignIn() {
-  const {
-    connected,
-    publicKey,
-  } = useWallet();
+  const { connected, publicKey, signMessage } = useWallet();
   const router = useRouter();
 
   // Redirect to dashboard when wallet connects
   useEffect(() => {
-    if (connected && publicKey) {
-      router.push('/dashboard');
+    if (!connected || !publicKey || !signMessage) return;
+
+    const createUser = async () => {
+      const addressFormData = new FormData();
+      addressFormData.append("address", publicKey.toString());
+
+      const signFormData = new FormData();
+      signFormData.append("address", publicKey.toString());
+
+      try {
+        const userResponse =
+          await fetch('https://buymeatoken-production.up.railway.app/api/user', {
+            method: "POST",
+            body: addressFormData
+          })
+
+        const message = await userResponse.json();
+        // useWallet's signMessage only takes in a uInt8Array param
+        // hence the econding with TextEncoder which returns a uInt8Array
+        const encodedMessage = new TextEncoder().encode(message.message);
+        const signature = await signMessage(encodedMessage);
+        
+        // Convert to base58 (proper Solana format)
+        const signatureBase58 = bs58.encode(signature);
+        signFormData.append("signature", signatureBase58);
+
+        await fetch('https://buymeatoken-production.up.railway.app/api/user/verify', {
+          method: "PUT",
+          body: signFormData
+        })
+
+        router.replace('/dashboard');
+      } catch (error) {
+        console.error("Sign in error: ", error);
+        // display toast notification
+      }
     }
-  }, [connected, publicKey, router]);
+
+    createUser();
+
+  }, [connected, publicKey, router, signMessage]);
 
   return (
     <div className="h-screen w-full relative overflow-clip">
